df9a43b9deb412d2bc41f3f38ca65a68437fd660
Dishes00 <nidshakya012@gmail.com>
2025-11-18 13:32:00 -0500

    light mode

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -14,9 +14,29 @@ for (let x = 0; x < chats.length; x++) {
 //Dark Mode Icon onclick
 
 function turnDark(){
+    const darkIcon = document.querySelector('#darkModeIcon');
+    const lightIcon = document.querySelector('#lightModeIcon');
+
     document.body.style.background = '#242424';
     const elements = document.querySelectorAll('.backgroundSections'); 
     Array.from(elements).forEach(element => {
         element.style.background = '#363636';
     });
+
+    darkIcon.style.display = 'none';
+    lightIcon.style.display = 'flex';
+}
+
+function turnLight(){
+    const darkIcon = document.querySelector('#darkModeIcon');
+    const lightIcon = document.querySelector('#lightModeIcon');
+
+    document.body.style.background = '#cccccc';
+    const elements = document.querySelectorAll('.backgroundSections'); 
+    Array.from(elements).forEach(element => {
+        element.style.background = '#e5e5e5';
+    });
+
+    darkIcon.style.display = 'flex';
+    lightIcon.style.display = 'none';
 }
